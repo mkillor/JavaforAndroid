b6093d77eb02dcf08f262fc746a79b802cee22d8
mkillor <mark.killoran.jr@gmail.com>
2018-01-22 22:14:52 -0500

    Initial commit

---
+++ src/com/killoran/week2/Main.java
@@ -0,0 +1,12 @@
+package com.killoran.week2;
+
+/**
+ * Created by MK on 1/22/2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+            System.out.println("Week 2!");
+
+    }
+}
