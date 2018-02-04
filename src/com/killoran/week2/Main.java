31e4ee151c9e1e7ac55b619eb46142181206a31c
mkillor <mark.killoran.jr@gmail.com>
2018-02-04 15:46:04 -0500

    Week 2 committing changes

--- src/com/killoran/week2/Main.java
+++ src/com/killoran/week2/Main.java
@@ -6,8 +6,34 @@ package com.killoran.week2;
 public class Main {
     public static void main(String[] args) {
 
-            System.out.println("Week 2!");
-            System.out.println("Week 2 test");
+        System.out.println("Week 2!");
+        System.out.println("Week 2 test");
+
+        boolean result = true && true;
+        System.out.println("true && true: " + result);
+
+        result = true && false;
+        System.out.println("true && false: " + result);
+
+        result = false && true;
+        System.out.println("false && true: " + result);
+
+        result = false && false;
+        System.out.println("false && false: " + result);
+
+
+        //The four combinations with logical inclusive OR
+        result = true | true;
+        System.out.println("true | true: " + result);
+
+        result = true | false;
+        System.out.println("true | false: " + result);
+
+        result = false | true;
+        System.out.println("false | true: " + result);
+
+        result = false | false;
+        System.out.println("false | false: " + result);
 
     }
 }
